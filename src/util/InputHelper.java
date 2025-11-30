221b1ae9deefd21c2ae0afeeb5fa728672bf06f5
Ivanrhmt77 <ivanrahmat.p77@gmail.com>
2025-11-30 22:46:07 +0700

    feat(util): add string input validation to InputHelper

--- src/util/InputHelper.java
+++ src/util/InputHelper.java
@@ -10,4 +10,18 @@ public class InputHelper {
             return -1;
         }
     }
+
+    public static String getString(Scanner scanner, String prompt) {
+        String input;
+        do {
+            System.out.print(prompt);
+            input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                System.out.println("\nInput tidak boleh kosong. Silakan coba lagi.");
+            }
+        } while (input.isEmpty());
+
+        return input;
+    }
 }
